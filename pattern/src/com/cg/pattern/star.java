1ffd1ad13eb15e869cd76bba379cb80023521089
Pramodh k <Pramodh k@Pramodh>
2022-07-28 12:51:08 +0530

    first commit

---
+++ pattern/src/com/cg/pattern/star.java
@@ -0,0 +1,15 @@
+package com.cg.pattern;
+
+public class star {
+
+	public static void main(String[] args) {
+		int r,c;
+		for(r=1;r<=r-2;r++) {
+			for(c=1;c<=5-r+2;c++) {
+				System.out.println("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
