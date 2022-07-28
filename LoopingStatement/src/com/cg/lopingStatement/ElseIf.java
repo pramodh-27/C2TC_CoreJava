bbc99d710dc4a91fc7d1df1eadf6baf9232fecd8
Pramodh k <Pramodh k@Pramodh>
2022-07-28 12:45:29 +0530

    first commit

---
+++ LoopingStatement/src/com/cg/lopingStatement/ElseIf.java
@@ -0,0 +1,23 @@
+package com.cg.lopingStatement;
+
+public class ElseIf {
+	public static void main(String[] args) {
+		String lang="java";
+	if(lang=="python") 
+		{
+			System.out.println("lang is python");
+			
+		}
+		else if(lang=="c++") 
+		{
+			System.out.println("lang is c++");
+			
+		}
+		else if(lang=="java");
+		{
+		System.out.println("lang is java");
+		}
+		
+		
+		System.out.println("lang");}
+}
