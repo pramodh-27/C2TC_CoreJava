bfdb43e64fe3b7fb0ba93edc072328bb2ea855fe
Pramodh k <Pramodh k@Pramodh>
2022-07-28 12:50:25 +0530

    first commit

---
+++ operators1/src/com/cg/operators1/Opera.java
@@ -0,0 +1,17 @@
+package com.cg.operators1;
+
+public class Opera {
+	public static void main(String args[]) {
+int a=5;
+int b=18;
+if((a=3)==b)
+{
+	System.out.println(++a);
+}
+else
+{
+	System.out.println(++b);
+}
+}
+}
+
